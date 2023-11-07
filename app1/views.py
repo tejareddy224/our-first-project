645c58c5adae4483a1da9bf481ee3f205f5dc846
tejareddy224 <tejareddy3916@gmail.com>
2023-11-07 22:04:49 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def JamPandu(request):
+    return HttpResponse<h1>('Hi This is Jampandu')
+
+def JigelRani(request):
+    return HttpResponse('Hi This is Jigelrani')
